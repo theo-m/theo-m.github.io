ca87b25a328a92bf68ca45472686f60be4ebf819
Théo Matussière <theo@matussie.re>
2021-12-06 16:26:46 +0100

    publish script

---
+++ publish.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+# https://unix.stackexchange.com/a/155077
+if [ -z "$(git status --porcelain)" ]; then
+  # Working directory clean
+  echo "git state ok, proceeding.."
+else
+  echo "git state is dirty - won't do anything until clean.."
+  git status
+  exit 1
+  # Uncommitted changes
+fi
+
+BUIDL=$(date +%s)
+cp -r out/ "/tmp/${BUIDL}"
+git checkout matussie.re
+git rm -rf *
+cp -r "/tmp/${BUIDL}/*" .
+git add -A
+git commit -m "update from nextjs"
+git push
