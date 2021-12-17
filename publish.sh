ce264b5c038d6e5d4640e443537aee9095c95da4
Théo Matussière <theo@matussie.re>
2021-12-17 16:24:56 +0100

    fake article

--- publish.sh
+++ publish.sh
@@ -7,20 +7,43 @@ if [ -z "$(git status --porcelain)" ]; then
 else
   echo "git state is dirty - won't do anything until clean.."
   git status
-  exit 1
+#  exit 1
   # Uncommitted changes
 fi
 
 BUIDL=$(date +%s)
+DIR="/tmp/theo-m-blog-build-${BUIDL}"
+CWD=$PWD
 set -Eex
 
 yarn build
-cp -r out/ "/tmp/${BUIDL}"
-git checkout matussie.re
-cp -r "/tmp/${BUIDL}/" .
-touch .nojekyll
-git add -A
-git commit -m "update from nextjs"
-git push
-git checkout new
 
+#cp -r out/ "/tmp/${BUIDL}"
+
+mkdir $DIR
+pushd $DIR
+
+git clone git@github.com:theo-m/theo-m.github.io.git .
+git checkout matussie.re
+
+cp -r "${CWD}"/out/* .
+touch .nojekyll
+
+git add -A && git commit -m "new build"
+git push
+
+popd
+
+rm -rf $DIR
+#
+#pushd "/tmp/${BUIDL}"
+#git init
+#
+#git checkout matussie.re
+#cp -r "/tmp/${BUIDL}/" .
+#touch .nojekyll
+#git add -A
+#git commit -m "update from nextjs"
+#git push
+#git checkout new
+#popd
