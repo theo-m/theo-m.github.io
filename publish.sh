83b8168856d3ebfb8e4ecd6019448d15994871fa
Théo Matussière <theo@matussie.re>
2021-12-17 16:26:59 +0100

    remove comment, faster clone

--- publish.sh
+++ publish.sh
@@ -2,13 +2,11 @@
 
 # https://unix.stackexchange.com/a/155077
 if [ -z "$(git status --porcelain)" ]; then
-  # Working directory clean
   echo "git state ok, proceeding.."
 else
   echo "git state is dirty - won't do anything until clean.."
   git status
-#  exit 1
-  # Uncommitted changes
+  exit 1
 fi
 
 BUIDL=$(date +%s)
@@ -18,13 +16,10 @@ set -Eex
 
 yarn build
 
-#cp -r out/ "/tmp/${BUIDL}"
-
 mkdir $DIR
 pushd $DIR
 
-git clone git@github.com:theo-m/theo-m.github.io.git .
-git checkout matussie.re
+git clone git@github.com:theo-m/theo-m.github.io.git --branch matussie.re --single-branch .
 
 cp -r "${CWD}"/out/* .
 touch .nojekyll
@@ -35,15 +30,3 @@ git push
 popd
 
 rm -rf $DIR
-#
-#pushd "/tmp/${BUIDL}"
-#git init
-#
-#git checkout matussie.re
-#cp -r "/tmp/${BUIDL}/" .
-#touch .nojekyll
-#git add -A
-#git commit -m "update from nextjs"
-#git push
-#git checkout new
-#popd
