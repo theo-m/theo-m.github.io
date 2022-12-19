eba07af789744188ac27c6636c437f4458ef0404
Théo Matussière <theo@matussie.re>
2022-12-19 17:47:50 +0100

    non blocking font

--- main.tsx
+++ main.tsx
@@ -161,7 +161,7 @@ const html = `
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;600&display=block" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;600&display=swap" rel="stylesheet">
     <style>
     body {
         font-family: Inter, Arial, sans-serif;
